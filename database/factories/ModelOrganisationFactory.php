dbf31a0a206bf19ffed56d010b9ba3e3c1beb5fe
Giorgi Magradze <giomagra@gmail.com>
2017-12-27 05:34:47 +0100

    Create Module -a

---
+++ database/factories/ModelOrganisationFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\Organisation::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -27,4 +16,10 @@ Route::group(['namespace' => 'Articles', 'prefix' => '/articles'], function (){
     Route::get('/{article}', ['as' => 'articles.show', 'uses' => 'ArticleController@show']);
     Route::post('/{article}', ['as' => 'articles.update', 'uses' => 'ArticleController@update']);
     Route::delete('/{article}', ['as' => 'articles.destroy', 'uses' => 'ArticleController@destroy']);
+});
+
+Route::apiResource('/organisations', 'OrganisationController');
+
+Route::group(['prefix'=>'organisation'], function (){
+    Route::apiResource('/{organisation}/reviews','ReviewController');
 });
