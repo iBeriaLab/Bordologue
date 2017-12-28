9ad409ae2601389837b9568bff3dc6dd8fc1fa85
Giorgi Magradze <giomagra@gmail.com>
2017-12-28 06:04:49 +0100

    Reviev Create

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Model\Organisation;
 class Review extends Model
 {
+    protected $fillable = ['star','customer','review'];
     public function organisation(){
         return $this->belongsTo(Organisation::class);
     }
